c8ec599509f876ccc5d7133b4bd9f1b9b5f2a19a
shiwangi880 <shiwangik078@gmail.com>
2026-01-25 16:52:14 +0530

    update backend requirements

--- backend/app.py
+++ backend/app.py
@@ -175,8 +175,10 @@ def text_mood():
         return jsonify({"error": str(e)}), 500
 
 # ------------------ Run Server ------------------
-if __name__ == "__main__":
+if __name__ == "__main__": 
     app.run(debug=True, host="0.0.0.0", port=7860)
+
+
 
 
 
